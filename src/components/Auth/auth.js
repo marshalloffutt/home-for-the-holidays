b529290d6fc1aad1a40f5822cac16c07891fb5b7
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-13 20:02:15 -0600

    finalizes login

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -2,15 +2,18 @@ import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import './auth.scss';
+import googleImage from './signin.png';
 
 const loginButton = () => {
   const domString = `
-    <button id="google-auth" class="btn btn-secondary">login</button>
+    <button id="google-auth" class="btn btn-secondary">
+      <img src="${googleImage}"/>
+    </button>
   `;
   $('#auth').html(domString);
   $('#google-auth').on('click', () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInPopUp(provider);
+    firebase.auth().signInWithPopup(provider);
   });
 };
 
