a35b24dadb8a07b7d6338382faf025310706e886
Marshall Offutt <marshalloffutt@gmail.com>
2018-11-19 19:15:56 -0600

    gets friends by uid

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import 'bootstrap';
 import './index.scss';
 import createNavbar from './components/Navbar/navbar';
 import loginButton from './components/Auth/auth';
-import checkLoginStatus from './helpers/authHelpers';
+import authHelpers from './helpers/authHelpers';
+import friendsPage from './components/FriendsPage/friendsPage';
 
 const initializeApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   createNavbar();
-  checkLoginStatus();
+  authHelpers.checkLoginStatus(friendsPage);
   loginButton();
 };
 
